32f1c0b391929208cd3bad7a923062675097f63a
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 01:42:32 +1100

    Add git config

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,8 @@ github_deploy_key=$(cat .ssh/id_rsa)
 eval "$(ssh-agent -s)"
 echo "$github_deploy_key" | ssh-add -
 
+git config --global user.email "zioroboco+release-bot@gmail.com"
+git config --global user.name "release-bot"
+git config --global commit.gpgsign false
+
 lerna version --conventional-commits --yes
