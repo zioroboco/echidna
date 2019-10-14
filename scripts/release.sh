c4e469e41b778d96a170ed523263c6a8b064ec29
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 01:54:49 +1100

    Target local registry for publish

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,4 +11,4 @@ git config --global user.name "release-bot"
 git config --global commit.gpgsign false
 
 lerna version --conventional-commits --create-release github --yes
-lerna publish from-git --yes
+lerna publish from-package --registry http://verdaccio:4873 --yes
