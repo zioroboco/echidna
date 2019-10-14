c97a3d64d883f6d08f3873804ccb45ac4cc054bb
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 01:37:11 +1100

    Get key and version

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e
 
-npm config list
+github_deploy_key=$(cat .ssh/id_rsa)
+
+eval "$(ssh-agent -s)"
+echo "$github_deploy_key" | ssh-add -
+
+lerna version --conventional-commits --yes
