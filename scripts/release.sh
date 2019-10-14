7e635b7379360f1138cd824627f339253646edfd
Louis Quinnell <zioroboco@gmail.com>
2019-10-15 02:14:46 +1100

    npm login

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,5 +10,7 @@ git config --global user.email "zioroboco+release-bot@gmail.com"
 git config --global user.name "release-bot"
 git config --global commit.gpgsign false
 
+npm login --registry http://verdaccio:4873
+
 lerna version --conventional-commits --create-release github --yes
 lerna publish from-package --registry http://verdaccio:4873 --yes
